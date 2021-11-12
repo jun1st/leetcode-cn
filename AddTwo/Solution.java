b8e00c4458f9220e5ebb8bdd30919e1a28d79b84
fengde <jun1st.feng@gmail.com>
2021-11-12 22:14:23 +0800

    add hash solution

--- AddTwo/Solution.java
+++ AddTwo/Solution.java
@@ -1,5 +1,8 @@
 package AddTwo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
 
     public static void main(String[] args) {
@@ -16,6 +19,7 @@ public class Solution {
 
     public int[] twoSum(int[] nums, int target) {
     
+        // 暴力搜索，最坏情况，搜两次，
         for (int i = 0; i < nums.length; i++) {
             int value = nums[i];
             for (int j = i+1; j < nums.length; j++) {
@@ -23,6 +27,22 @@ public class Solution {
                     return new int[]{i, j};
                 }
             }
+        }
+
+        return new int[0];
+    }
+
+    // 第一个访问的问题在于，对于每一个 X，都要依次遍历所有的元素来查抄 target - X
+    // 所以，提高效率的方法就在于把查找 target - X 的时间，缩短为 O(1), 利用 hash 表
+    public int[] twoHashSum(int[] nums, int target) {
+
+        Map<Integer, Integer> table = new HashMap<Integer, Integer>();
+
+        for(int i=0; i < nums.length; i++) {
+            if (table.containsKey(nums[i])) {
+                return new int[]{i, table.get(nums[i])};
+            }
+            table.put(nums[i], i);
         }
 
         return new int[0];
